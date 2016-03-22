7178e0dfdea4efcc646e518f1246046c32438f0b
Sai Harsh <saiharsh.t13@iiits.in>
2016-03-22 19:52:56 +0530

    Delete Number_of_Paths.r

--- Assignments/Assignments/Number_of_Paths.r
+++
@@ -1,9 +0,0 @@
-Number_of_Paths<-function(m,n){
-if((m == 1) || (n == 1))
-        return (1);
- 
-return  (Number_of_Paths(m-1, n) + Number_of_Paths(m, n-1));
-}
-Input<- readline(prompt="Please enter the two number which are separated by space")
-Input <- as.numeric(strsplit(as.character(Input)," ")[[1]])
-print(Number_of_Paths(Input[1],Input[2]))
